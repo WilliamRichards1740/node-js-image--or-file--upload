88e36e533a0e05c995f3eae6717f2b1647355bd0
WilliamRichards1740 <63824470+WilliamRichards1740@users.noreply.github.com>
2020-07-03 13:45:55 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const app = express();
+
+
+app.use(fileUpload());
+app.get('/', function(req, res) {
+res.sendFile(__dirname + '/index.html')
+
+})
+
+app.post('/upload', function(req, res) {
+   
+const sampleFile = req.files.upload;
+const name = req.files.upload.name;
+
+if (name == 'index.html'){
+
+  res.send("Please Dont Send Index Files!");
+}
+if (name == 'index.php'){
+
+  res.send("Dont You know Node js cant process PHP you silly goose :)");
+}
+
+
+
+if (name.endsWith(".html") == true){
+
+  res.send("Please Dont upload Html");
+   
+}
+if (name.endsWith(".htm") == true ){
+
+  res.send("Please Dont upload Html");
+}
+if (name.endsWith(".php") == true){
+
+  res.send("Dont You know Node js cant process PHP you silly goose :)");
+}
+
+
+  
+  else{
+  sampleFile.mv(__dirname + '/uploads/' + name , function(err) {
+    if (err)
+      res.send("The Server is Overloaded");
+
+    res.send('Thank You For the Contribution :)');
+  });
+}
+});
+console.log("Server Running");
+app.listen(4000);
